35b35df855d332922562175f62e1acc5535493c3
Quế Hương <luquehuong@gmail.com>
2018-09-28 15:46:48 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
   </div>
 </template>
+import TodoList from './components/TodoList'
+import CreateTodo from './components/CreateTodo'
 
 <script>
+import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateTodo";
+
 export default {
-  name: 'App'
-}
+  name: "app",
+  components: {
+    TodoList,
+    CreateTodo
+  },
+  // data function avails data to the templete
+  data() {
+    return {
+      todos: [
+        {
+          title: "Todo A",
+          project: "Project A",
+          done: false
+        },
+        {
+          title: "Todo B",
+          project: "Project B",
+          done: false
+        },
+        {
+          title: "Todo C",
+          project: "Project C",
+          done: false
+        }
+      ]
+    };
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title,
+        done: false
+      });
+    }
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
